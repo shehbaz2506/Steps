91b46125c0264e89c3a783e4cdc352fffa01a1be
shehbaz2506 <shehbazsinghchauhan878@gmail.com>
2026-05-31 17:24:42 +0530

    Added Event Handlers

--- src/App.js
+++ src/App.js
@@ -7,6 +7,17 @@
 export default function App(){
 
   const step =1;
+
+
+  function handlePrevious(){
+    alert("Previous")
+  }
+
+  function handleNext(){
+    alert("Next")
+  }
+
+
   return(
     <div className="steps">
       <div className="numbers">
@@ -18,8 +29,8 @@ export default function App(){
       <p className="message">Step {step}:{messages[step-1]}</p>
 
       <div className="buttons">
-        <button style={{backgroundColor:"#7950f2", color:"#fff"}} onClick={()=>alert("Button CLicked")}>Previous</button>
-        <button style={{backgroundColor:"#7950f2", color:"#fff"}}>Next</button>
+        <button style={{backgroundColor:"#7950f2", color:"#fff"}} onClick={handlePrevious}>Previous</button>
+        <button style={{backgroundColor:"#7950f2", color:"#fff"}} onClick={handleNext}>Next</button>
       </div>
     </div>
   )
