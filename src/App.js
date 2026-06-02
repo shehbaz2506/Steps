b90aa089d72b438d4824542fa0c696377ffac609
shehbaz2506 <shehbazsinghchauhan878@gmail.com>
2026-06-03 00:00:57 +0530

    Setting States Dynamically

--- src/App.js
+++ src/App.js
@@ -13,27 +13,32 @@ export default function App(){
   // step-3 then update the peice of code in event handler
 
   const [step, setStep ] = useState(1); // default value of use state is given 1  inside paranthesis
+  const [test, setTest] = useState({name:"Shehbaz"})
 
 
   function handlePrevious(){
     if(step >1) setStep(step-1)
+      
   }
 
   function handleNext(){
-
     if(step < 3) setStep(step +1)
+    setTest({name:"Shehbaz"})
   }
 
 
   return(
     <div className="steps">
       <div className="numbers">
-        <div className={`${step >=1 ? "active":""}`}>1</div>
-        <div className={`${step >=2 ? "active":""}`}>2</div>
-        <div className={`${step >=3 ? "active":""}`}>3</div>
+        <div className={step >=1 ? "active":""}>1</div>
+        <div className={step >=2 ? "active":""}>2</div>
+        <div className={step >=3 ? "active":""}>3</div>
       </div>
 
-      <p className="message">Step {step}:{messages[step-1]}</p>
+      <p className="message">
+        Step {step}:{messages[step-1]}
+        {test.name}
+        </p>
 
       <div className="buttons">
         <button style={{backgroundColor:"#7950f2", color:"#fff"}} onClick={handlePrevious}>Previous</button>
